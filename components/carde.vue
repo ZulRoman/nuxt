68e5fd317aabaf6ea96354d9408b618db9ae9f00
Zulfikar <zulfikarromansyah065@gmail.com>
2023-05-26 14:23:15 +0700

    abc

--- components/carde.vue
+++ components/carde.vue
@@ -7,32 +7,14 @@
                 logements compris</p>
         </div>
         <div class="grid lg:grid-cols-3 sm:grid-cols-2 gap-5 w-full">
-            <div class="flex flex-col gap-3 rounded hover:cursor-pointer">
+            <div class="flex flex-col gap-3 rounded hover:cursor-pointer"  v-for="item in ex.e" :key="item.id">
                 <div class="flex justify-center items-center h-64 bg-[url('/img/j2.png')] bg-no-repeat bg-cover bg-center hover:saturate-150 rounded ">
                     <span><img src="/img/lg.png" alt=""></span>
                 </div>
                 <h3 class="text-[14px] mt-1 font-bold text-fuchsia-900">
-                    Plus de 200 séjours vérifiés
+                    {{ item.name }}
                 </h3>
-                <p>À partir de 577€/personne - 3 jours</p>
-            </div>
-            <div class="flex flex-col gap-3 rounded hover:cursor-pointer">
-                <div class="flex justify-center items-center h-64 bg-[url('/img/j3.png')] bg-no-repeat bg-cover bg-center hover:saturate-150 rounded ">
-                    <span><img src="/img/lg.png" alt=""></span>
-                </div>
-                <h3 class="text-[14px] mt-1 font-bold text-fuchsia-900">
-                    Plus de 200 séjours vérifiés
-                </h3>
-                <p>À partir de 577€/personne - 3 jours</p>
-            </div>
-            <div class="flex flex-col gap-3 rounded hover:cursor-pointer">
-                <div class="flex justify-center items-center h-64 bg-[url('/img/j4.png')] bg-no-repeat bg-cover bg-center hover:saturate-150 rounded ">
-                    <span><img src="/img/lg.png" alt=""></span>
-                </div>
-                <h3 class="text-[14px] mt-1 font-bold text-fuchsia-900">
-                    Plus de 200 séjours vérifiés
-                </h3>
-                <p>À partir de 577€/personne - 3 jours</p>
+                <p>{{ item.price }}</p>
             </div>
         </div>
     </div>
