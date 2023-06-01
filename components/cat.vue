f5ef137094d34c631824e3b60ff4a3ce07458424
Zulfikar <zulfikarromansyah065@gmail.com>
2023-06-01 20:36:20 +0700

    bit

--- components/cat.vue
+++ components/cat.vue
@@ -7,7 +7,7 @@ const { data: data } = await useFetch('https://dummyjson.com/products/categories
         <div class="flex justify-center mb-2 mt-[88px] text-4xl font-bold">
             <h1>Product Categories</h1>
         </div>
-    <div class="mx-[80px] bg-gray-900 text-white max-w-[1185px]">
+    <div class="mx-[80px] bg-gray-900 text-white max-w-[1100px]">
         <div class="flex justify-center">
             <div class="flex gap-2 mx-2 overflow-x-scroll" >
                 <div class="flex flex-col w-[109px] justify-center items-center h-14 hover:cursor-pointer hover:scale-110 duration-1000 my-4"
