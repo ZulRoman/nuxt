4eaad9e0dd0f83395d1ef1d63c3f8a6be67134ae
Zulfikar <zulfikarromansyah065@gmail.com>
2023-06-06 11:55:09 +0700

    fix

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,10 +2,10 @@
     <div class="flex items-center sm:px-10 px-2 h-20 w-full sticky top-0 border-b bg-slate-900 z-10 text-slate-950">
         <div class="w-full flex justify-between mx-auto font-semibold text-lg">
             <div class="flex flex-row sm:justify-start items-center w-full" >  
-                <NuxtLink to="/">
+                <NuxtLink to="/list">
                     <img src="/svg/airbnb.svg" alt="AirBnBlogo" srcset="" class="h-8 lg:block hidden">
                 </NuxtLink>
-                <NuxtLink to="/">
+                <NuxtLink to="/list">
                     <img src="/svg/airbnb-1.svg" alt="AirBnBlogo" srcset="" class="h-10 lg:hidden sm:block mr-2">
                 </NuxtLink>
                 <Search />
