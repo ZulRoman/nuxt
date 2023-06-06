ec3471a5accf61e0b2d8c6ec9413a7de8547ee25
Zulfikar <zulfikarromansyah065@gmail.com>
2023-06-06 15:17:26 +0700

    fix

--- pages/list/[id].vue
+++ pages/list/[id].vue
@@ -20,7 +20,6 @@ const { data: ex } = await useFetch('https://dummyjson.com/products/' + id)
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
   <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
 </svg>
-
                 <span class="-ml-1">{{ ex.rating }}</span> -
                 <span class="underline">{{ ex.stock }} Stok tersedia</span> -
                 <span class="underline">{{ ex.brand }}</span>
@@ -311,7 +310,6 @@ export default {
             dataProduct.qty        = qty;
             dataProduct.noteStatus = false;
 
-            //if localStorage products didn't exist
             if (!localStorage.getItem("products")) {
                 let array = [];
                 dataProduct.cartId = 1;
@@ -320,12 +318,9 @@ export default {
                 localStorage.setItem("products", JSON.stringify(array));
                 console.log(JSON.parse(localStorage.getItem("products")));
             } 
-            //if localStorage products is exist
             else {
-                //get all data from localStorage
                 let productAtCart = JSON.parse(localStorage.getItem("products"));
                 let listProductId = [];
-                //get ListProductId in localStorage
                 for (let i = 0; i < productAtCart.length; i++) {
                     if (!listProductId.includes(productAtCart[i].id)) {
                         listProductId.push(productAtCart[i].id)
@@ -333,9 +328,7 @@ export default {
                 }
                 console.log(listProductId);
                 
-                //check if the id already exists or not
                 if (!listProductId.includes(dataProduct.id)) {
-                    //if not exist save to localStorage
                     dataProduct.cartId   = productAtCart.length + 1;
                     dataProduct.subTotal = (this.price * this.stockCounter) + this.cleaningPrice + this.adminPrice;
                     productAtCart.push(dataProduct);
@@ -343,10 +336,7 @@ export default {
                     console.log(JSON.parse(localStorage.getItem("products")));
                     console.log(listProductId);
                 } else {
-                    //if exist
-                    //get data with the same id from localStorage
                     for (let j = 0; j < productAtCart.length; j++) {
-                        //if data found
                         if (productAtCart[j].id === id) {
                             productAtCart[j].subTotal += (this.price * this.stockCounter) + this.cleaningPrice + this.adminPrice;
                             productAtCart[j].qty      += qty;
